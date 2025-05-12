f6b64f65d1391f6530cef853a75145aaa082dc7b
Nicolas <nicolasgt@al.insper.edu.br>
2025-05-12 17:00:50 -0300

    tentativa de gráfico conclusivo

---
+++ Conclusivo.py
@@ -0,0 +1,19 @@
+import Modelo as m
+import numpy as np
+import matplotlib.pyplot as plt
+from math import *
+from scipy.integrate import odeint
+
+lista_L = np.arange(0.5, 30, 0.5)
+
+for L in lista_L:
+    result = odeint(m.modelo2,m.z0, m.tempo, args=(L,))
+    solx = result[:,0]
+    soly = result[:,1]
+    svxs = result[:,2]
+    svys = result[:,3]
+    plt.plot(m.tempo,svxs)
+plt.grid()
+plt.xlabel('tempo')
+plt.ylabel('velocidade em x')
+plt.show()
--- Modelo.py
+++ Modelo.py
@@ -47,7 +47,7 @@ def modelo(z, t):
     return dzdt
 
 #implementar
-tempo = np.arange(0, 5, 1e-3)
+tempo = np.arange(0, 10, 1e-3)
 x0 = L*sin(radians(45))
 y0 = -L*cos(radians(45))
 vx0 = 0
@@ -67,3 +67,38 @@ plt.ylabel('posições em Y')
 plt.grid()
 plt.axis('equal')
 plt.show()
+
+def modelo2(z, t, Lenght):
+    
+    Sx = z[0]
+    Sy = z[1]
+    vx = z[2]
+    vy = z[3]
+    v = ((vx**2) + (vy**2))**(1/2)
+    v2 = v**2
+    if v > 0:
+        Far = (Cd*p*A*(v2))/2
+        cos_a = (-vx/v)
+        sen_a = (-vy/v)
+            
+    else:
+        Far = 0
+        cos_a = 0
+        sen_a = 0
+        
+      
+    sen_0 = (Sx/Lenght)
+    cos_0 = (-Sy/Lenght)
+    P = m*g 
+    
+    T = ((m*(v2)/Lenght) + (P*cos_0))
+    
+    Rx = Far*cos_a - T*sen_0
+    Ry = T*cos_0 - P + Far*sen_a
+
+    dxdt = vx
+    dydt = vy
+    dvxdt = Rx/m
+    dvydt = Ry/m
+    dzdt = [dxdt, dydt, dvxdt, dvydt]
+    return dzdt
