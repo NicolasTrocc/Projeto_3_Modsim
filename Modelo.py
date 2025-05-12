023e86dbad5b28c5f3964c78341069a55af131c5
Nicolas <nicolasgt@al.insper.edu.br>
2025-05-12 15:09:51 -0300

    Coisass

--- Modelo.py
+++ Modelo.py
@@ -6,16 +6,12 @@ from scipy.integrate import odeint
 
 #parametros
 
-m = 0
+m = 75
 g = 9.8
-p = 0
-Cd = 0
-Area = 0
+p = 1.2
+Cd = 1.05
+Area = 0.965
 L = 1
-
-
-
-
 
 #modelo
 def modelo(x, t):
@@ -26,7 +22,6 @@ def modelo(x, t):
     vy = x[3]
     v = ((vx**2) + (vy**2))**(1/2)
 
-
     Far = (1/2)*Cd*p*Area*(v**2)
     cos_0 = (vx/v)
     sen_0 = (vy/v)
@@ -34,7 +29,6 @@ def modelo(x, t):
 
     T = ((m*(v**2)/L) + (P*cos_0))
     
-
     Rx = -(Far*cos_0) + (T*sen_0)
     Ry = (T*cos_0) - P + (Far*sen_0)
 
